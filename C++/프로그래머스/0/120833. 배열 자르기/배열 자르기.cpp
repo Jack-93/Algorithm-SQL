b7ef0ef374e54136164efd258ef3979a5a28a192
Sparrow <94902059+Jack-93@users.noreply.github.com>
2024-04-16 04:49:56 +0900

    [level 0] Title: 배열 자르기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120833. 배열 자르기/배열 자르기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers, int num1, int num2) {
+    vector<int> answer(numbers.begin()+num1, numbers.begin()+num2+1);
+
+    return answer;
+}
