c49bb2c3c69dd79b2e0ef4de6aaa86862db73262
Sparrow <94902059+Jack-93@users.noreply.github.com>
2024-04-21 08:50:04 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> dp(n + 1, 0);
+    for (int i = 2; i <= n; ++i)
+    {
+        dp[i] = dp[i - 1] + 1;
+        if (i % 2 == 0)
+            dp[i] = min(dp[i], dp[i / 2] + 1);
+        if (i % 3 == 0)
+            dp[i] = min(dp[i], dp[i / 3] + 1);
+    }
+    cout << dp[n] << endl;
+
+    return 0;
+}
