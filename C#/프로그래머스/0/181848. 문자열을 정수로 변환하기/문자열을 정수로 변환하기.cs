cf70929b96536294e6800929d7bd4b4ea528c380
Sparrow <94902059+Jack-93@users.noreply.github.com>
2024-04-22 20:04:14 +0900

    [level 0] Title: 문자열을 정수로 변환하기, Time: 0.61 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181848. 문자열을 정수로 변환하기/문자열을 정수로 변환하기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(string n_str) {
+        int answer = 0;
+        
+        answer = int.Parse(n_str);
+        
+        return answer;
+    }
+}
