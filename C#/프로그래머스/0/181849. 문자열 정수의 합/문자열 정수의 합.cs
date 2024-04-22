60d35506c9bf54b21f2586588804bebd2c0f8692
Sparrow <94902059+Jack-93@users.noreply.github.com>
2024-04-22 20:08:47 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.17 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(string num_str) {
+        int answer = 0;
+        for(int i=0;i<num_str.Length;i++){
+            answer += num_str[i] - 48;
+        }
+        return answer;
+    }
+}
