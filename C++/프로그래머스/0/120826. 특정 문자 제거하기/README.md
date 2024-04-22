ff93011a757953ffdfd2b10f0561466c92d8fdaf
Sparrow <94902059+Jack-93@users.noreply.github.com>
2024-04-23 08:01:40 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- C++/프로그래머스/0/120826. 특정 문자 제거하기/README.md
+++ C++/프로그래머스/0/120826. 특정 문자 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 특정 문자 제거하기 - 120826 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120826?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120826) 
 
 ### 성능 요약
 
-메모리: 4.13 MB, 시간: 0.01 ms
+메모리: 4.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 16일 04:31:31
+2024년 04월 23일 08:01:38
 
 ### 문제 설명
 
--- C++/프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
+++ C++/프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
@@ -8,8 +8,15 @@ string solution(string my_string, string letter) {
     string answer = "";
     string empty_str = "";
     
-    for (auto i: letter) {
-        my_string.erase(remove(my_string.begin(), my_string.end(), i), my_string.end());
+    // for (auto i: letter) {
+    //     my_string.erase(remove(my_string.begin(), my_string.end(), i), my_string.end());
+    // }
+    while(true){
+        size_t pos = my_string.find(letter);
+        if(pos!=string::npos){
+            my_string.erase(pos, 1);
+        }
+        else break;
     }
     answer = my_string;
     return answer;
