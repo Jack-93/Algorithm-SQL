a9a0b9148033539a9c22a17fbe0996acc90af96b
Sparrow <94902059+Jack-93@users.noreply.github.com>
2024-04-22 10:50:42 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 212 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int count1 = 0;
+int count2 = 0;
+
+int fib(int n)
+{
+    count1++;
+    if (n == 1 or n == 2)
+        return 1;
+    else
+        return (fib(n - 1) + fib(n - 2));
+}
+int fibonacci(int n)
+{
+    int arr[41];
+    for (int i = 3; i <= n; i++)
+    {
+        count2++;
+        arr[i] = arr[i - 2] + arr[i - 1];
+    }
+    return count2;
+}
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << fib(n) << " " << fibonacci(n);
+}
